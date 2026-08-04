c55a0aedd5bf666137ff21aff09fc1813874467d
Tenfin9ers <myersandrew99@gmail.com>
2026-08-03 22:20:37 -0500

    heart and tools as sections in index, redirect stubs for old pages

--- config.js
+++ config.js
@@ -2,7 +2,10 @@ window.CONFIG = {
   CHAIN_ID: "0x1",
   RPC_ENDPOINTS: [
     "https://eth-mainnet.g.alchemy.com/v2/alch_3fiqFg8RuxP3NIcBqSEz5",
-    "https://ethereum-rpc.publicnode.com"
+    "https://ethereum-rpc.publicnode.com",
+    "https://eth.llamarpc.com",
+    "https://rpc.ankr.com/eth",
+    "https://cloudflare-eth.com"
   ],
   CONTRACT: "0xEB46ebc19dB1814B6F72eD68ec752631B8F4FF2C",
   RENDERER: "0x18aCbF40FF1C2Ae97D11fc7d895AF01254116239"
